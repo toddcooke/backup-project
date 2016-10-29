6664f39066127e75d0363c0e5a20a8fd1405c171
Todd Cooke <toddcookevt@gmail.com>
2016-10-29 17:11:14 -0400

    UI now uses html rather than javaFX.

---
+++ src/code/Main.java
@@ -0,0 +1,55 @@
+package code;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.awt.*;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.URI;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Main {
+    
+    public static void main(String[] args) throws Exception {
+        // Test to ensure file is being read
+//        String response = readFile("../../html/backup.html", Charset.defaultCharset());
+//        System.out.println(response);
+
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        server.createContext("/index", new MyHandler());
+        server.setExecutor(null); // creates a default executor
+        server.start();
+
+
+        // Launch tab or window
+        if (Desktop.isDesktopSupported()) {
+            Desktop.getDesktop().browse(new URI("http://localhost:8000/index"));
+        }
+    }
+
+    private static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+
+            String response = readFile("../../html/backup.html", Charset.defaultCharset());
+
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+
+    private static String readFile(String path, Charset encoding)
+            throws IOException {
+        byte[] encoded = Files.readAllBytes(Paths.get(path));
+        return new String(encoded, encoding);
+    }
+
+}
