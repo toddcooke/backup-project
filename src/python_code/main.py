f149c4c208d22a9c497ae2e58961438a5da3a143
todd <toddcookevt@gmail.com>
2016-11-11 17:56:40 -0500

    progress with database insert

--- src/python_code/main.py
+++ src/python_code/main.py
@@ -4,9 +4,10 @@ import sqlite3
 from sqlite3 import OperationalError
 from bottle import route, run, template, request, static_file, error
 
-db_name = 'backup_info'
+db_backup_info = 'backup_info'
+db_backup_schedule = 'backup_schedule'
 backup_repository = 'backup_repository'
-backup_schedule = 'backup_schedule'
+msg_success = 'Item backed up successfully'
 
 
 @route('/static/<filename>')
@@ -23,10 +24,10 @@ def main_page():
 def restore_backup():
     path = request.GET.file_path.strip()
 
-    if not path:
-        return template('html/restore_backup', msg='')
+    # if not path:
+    #     return template('html/restore_backup', msg='')
 
-    select = database_query("select * from {}".format(backup_schedule))
+    select = database_query("select * from {}".format(db_backup_schedule))
     # if select[0] == 'error':
     #     return template('html/restore_backup', msg='Error: ' + select[-1])
     return template('html/restore_backup', msg=select)
@@ -37,12 +38,20 @@ def regular_backup(item):
     return template('html/{}'.format(item), msg='')
 
 
-def database_query(query):
-    conn = sqlite3.connect(db_name + '.db')
+def database_query(query, values):
+    conn = sqlite3.connect(db_backup_info + '.db')
     c = conn.cursor()
 
+    TEST = (None,
+            '/home/todd/ok.txt',
+            'montue',
+            '15:30',
+            1,
+            0,
+            0)
+
     try:
-        c.execute(query)
+        c.execute(query, TEST)
     except OperationalError as e:
         return ['error', 'Query returned an error: ', e]
 
@@ -60,16 +69,25 @@ def copy_item_to_repo(item):
 
 @route('/schedule_<kind>_backup', method='GET')
 def new_item(kind):
+    days = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat']
     path = request.GET.file_path.strip()
-    # days = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat']
 
-    # Backup and make entry in DB
-    if os.path.exists(path):
-        # database_query('insert into {} VALUES ()')
-        return template('html/schedule_{}_backup'.format(kind), msg='Item backed up successfully.')
-    # Return to body with error message
-    else:
+    if not os.path.exists(path):
         return template('html/schedule_{}_backup'.format(kind), msg='Error: The path specified was invalid.')
+
+    if kind == 'weekly':
+        checked_days = '_'.join([i for i in request.query.keys() if i in days])
+        result = database_query('INSERT INTO {} VALUES (?,?,?,?,?,?,?)'.format(db_backup_schedule),
+                                (None, path, checked_days, request.GET.time.strip(),
+                                 True, False, False))
+
+        return template('html/schedule_{}_backup'.format(kind), msg=result)
+    elif kind == 'monthly':
+        pass
+    elif kind == 'custom':
+        pass
+    else:
+        pass
 
 
 @error(404)
